a15b6e69415ae2a81d279a1618551c01ab29f094
Deepak Chhikara <dpk.chhikara@gmail.com>
2022-12-27 13:43:27 +0530

    Complete

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ def welcome():
 # the data which the user inputs
 def prediction(variance,skewness,curtosis,entropy):  
    
-    prediction = model.predict(
-        [['variance','skewness','curtosis','entropy']])
+    prediction = model.predict([[variance,skewness,curtosis,entropy]])
     print(prediction)
     return prediction
       
@@ -29,8 +28,8 @@ def main():
     # here we define some of the front end elements of the web page like 
     # the font and background color, the padding and the text to be displayed
     html_temp = """
-    <div style ="background-color:yellow;padding:13px">
-    <h1 style ="color:black;text-align:center;">Streamlit Fake Currency ML App </h1>
+    <div style ="background-color:black;padding:13px">
+    <h1 style ="color:white;text-align:center;">The Magical App </h1>
     </div>
     """
       
@@ -40,10 +39,10 @@ def main():
       
     # the following lines create text boxes in which the user can enter 
     # the data required to make the prediction
-    variance = st.text_input("Variance", "")
-    skewness = st.text_input("Skewness", "")
-    curtosis = st.text_input("Curtosis", "")
-    entropy = st.text_input("Entropy", "")
+    variance = st.slider("Variance", min_value=-20.0, max_value=20.0)
+    skewness = st.slider("Skewness", min_value=-20.0, max_value=20.0)
+    curtosis = st.slider("Curtosis", min_value=-20.0, max_value=20.0)
+    entropy = st.slider("Entropy", min_value=-20.0, max_value=20.0)
     result =""
       
     # the below line ensures that when the button called 'Predict' is clicked, 
@@ -51,9 +50,10 @@ def main():
     # and store it in the variable result
     if st.button("Predict"):
         result = prediction(variance,skewness,curtosis,entropy)
-    if result == 0:
-        st.success('The Currency is Fake')
-    else:
-        st.success('The Currency is Real')
+        if result == 0:
+            st.success('This is a Fake Currency')
+        elif result == 1:
+            st.success('This is a Real Currency')
+     
 if __name__=='__main__':
     main()
