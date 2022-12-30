cc5a00f741ab35eac1738cc6fd7318356f1225fc
Deepak Chhikara <dpk.chhikara@gmail.com>
2022-12-30 10:45:12 +0530

    Initial Streamlit

--- app.py
+++ app.py
@@ -2,58 +2,78 @@ import pandas as pd
 import numpy as np
 import pickle
 import streamlit as st
+from streamlit_option_menu import option_menu
 from PIL import Image
+import database as db
   
 # loading in the model to predict on the data
 pickle_in = open('model.pkl', 'rb')
 model = pickle.load(pickle_in)
-  
-def welcome():
-    return 'welcome all'
-  
-# defining the function which will make the prediction using 
-# the data which the user inputs
-def prediction(variance,skewness,curtosis,entropy):  
-   
+
+#Our prediction model
+def prediction(variance,skewness,curtosis,entropy):
     prediction = model.predict([[variance,skewness,curtosis,entropy]])
     print(prediction)
     return prediction
-      
-  
-# this is the main function in which we define our webpage 
-def main():
-      # giving the webpage a title
-    st.title("Fake Currency Prediction")
-      
-    # here we define some of the front end elements of the web page like 
-    # the font and background color, the padding and the text to be displayed
-    html_temp = """
-    <div style ="background-color:black;padding:13px">
-    <h1 style ="color:white;text-align:center;">The Magical App </h1>
-    </div>
+
+
+page_icon = ":dollar:"
+page_title = "Fake Currency Detection"
+layout = "centered"
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title + " " + page_icon)
+
+
+# --- DATABASE INTERFACE ---
+def get_all_periods():
+    items = db.fetch_all_periods()
+    periods = [item["key"] for item in items]
+    return periods
+
+
+#Hide Streamlit Style
+hide_st_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer{visibility: hidden;}
+    header{visibility: hidden;}
+    </style>
     """
-      
-    # this line allows us to display the front end aspects we have 
-    # defined in the above code
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+#Navigation Menu
+selected = option_menu(
+    menu_title = None,
+    options=["Prediction Centre","History"],
+    icons =["bank","archive"],
+    orientation="horizontal")
+
+if selected == "Prediction Centre":
+    html_temp = """
+        <div style ="background-color:black;padding:13px">
+        <h1 style ="color:yellow;text-align:center;">Currency - Real or Fake</h1>
+        </div>
+        """      
     st.markdown(html_temp, unsafe_allow_html = True)
-      
-    # the following lines create text boxes in which the user can enter 
-    # the data required to make the prediction
-    variance = st.slider("Variance", min_value=-20.0, max_value=20.0)
-    skewness = st.slider("Skewness", min_value=-20.0, max_value=20.0)
-    curtosis = st.slider("Curtosis", min_value=-20.0, max_value=20.0)
-    entropy = st.slider("Entropy", min_value=-20.0, max_value=20.0)
-    result =""
-      
-    # the below line ensures that when the button called 'Predict' is clicked, 
-    # the prediction function defined above is called to make the prediction 
-    # and store it in the variable result
-    if st.button("Predict"):
-        result = prediction(variance,skewness,curtosis,entropy)
-        if result == 0:
-            st.success('This is a Fake Currency')
-        elif result == 1:
-            st.success('This is a Real Currency')
-     
-if __name__=='__main__':
-    main()
+
+
+    with st.form("entry_form", clear_on_submit=True):
+        # the following lines create text boxes in which the user can enter 
+        # the data required to make the prediction
+        variance = st.slider("Variance", -20, 20, 0)
+        skewness = st.slider("Skewness", -20, 20, 0)
+        curtosis = st.slider("Curtosis", -20, 20, 0)
+        entropy = st.slider("Entropy", -20, 20, 0)
+        "---"
+        submitted = st.form_submit_button("Predict")
+        if submitted:
+            db.insert_period(variance,skewness,curtosis,entropy)
+            result = prediction(variance,skewness,curtosis,entropy)
+            if result == 0:
+                st.success('This is a Fake Currency')
+            elif result == 1:
+                st.success('This is a Real Currency')
+
+
+if selected == "Prediction Centre":
+    st.markdown("Database feature is under-development. Sorry for the inconvience.")
